f366e16fa926bdab9bc075b169296d6670432a31
javicastillo245 <jcg0075@alu.medac.es>
2025-05-15 23:04:56 +0200

    Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -2,23 +2,23 @@
 package trabajoEntornos;
 
 public class Empleado {
-
+    //Declaramos Variables
     private int id;
     private String nombre;
     private String cargo;
     private int DNI;
-
+    //Método constructor
     public Empleado(int id, String nombre, String cargo, int DNI) {
         this.id = id;
         this.nombre = nombre;
         this.cargo = cargo;
         this.DNI = DNI;
     }
-
+    //Indica quien procesa la venta
     public void procesarVentas() {
         System.out.println("El empleado " + nombre + " está procesando ventas.");
     }
-
+    //Get y Set
     public int getDNI() {
         return DNI;
     }
@@ -26,7 +26,7 @@ public class Empleado {
     public void setDNI(int DNI) {
         this.DNI = DNI;
     }
-
+    //Indica a que puesto asciende
     public void ascenderPuesto() {
         if (cargo.equals("Vendedor")) {
             cargo = "Jefe de Ventas";
