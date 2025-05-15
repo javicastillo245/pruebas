d22d8213205a3c96948a8027f9692ab1e6ebffe8
javicastillo245 <jcg0075@alu.medac.es>
2025-05-15 22:46:47 +0200

    Compra.java

--- Compra.java
+++ Compra.java
@@ -1,13 +1,13 @@
 package trabajoEntornos;
 
 public class Compra {
-
+    //Inicializamos variables
     private int id;
     private String fecha;
     private float total;
     private int cantProductos;
     private Producto[] productos;
-
+    //metodo constructor
     public Compra(int id, String fecha) {
         this.id = id;
         this.fecha = fecha;
@@ -15,7 +15,7 @@ public class Compra {
         this.cantProductos = 0;
         this.productos = new Producto[100];
     }
-
+    //Método para agrigar productos
     public void agregarProducto(Producto p) {
         if (cantProductos < productos.length) {
             productos[cantProductos] = p;
@@ -23,7 +23,7 @@ public class Compra {
             cantProductos++;
         }
     }
-
+    //Método que calcula el total de la compra
     public float calcularTotal() {
         float suma = 0;
         for (int i = 0; i < cantProductos; i++) {
@@ -32,12 +32,14 @@ public class Compra {
         total = suma;
         return total;
     }
+    //Get y set
     public int getCantProductos() {
         return cantProductos;
     }
     public void setCantProductos(int cantProductos) {
         this.cantProductos = cantProductos;
     }
+    //Indica el total sumado al IVA
     public float totalIva() {
         return (float) (total * 1.21); 
     }
