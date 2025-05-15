8dbbbccb1c86c2cde2ce723a049aa10c377fb7fa
javicastillo245 <jcg0075@alu.medac.es>
2025-05-15 22:44:55 +0200

    Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -2,22 +2,23 @@
 package trabajoEntornos;
 
 public class Cliente {
+    //Inicializamos las variables
     private int id;
     private String nombre;
     private String correo;
     private String telefono;
-
+    //Método constructor
     public Cliente(int id, String nombre, String correo, String telefono) {
         this.id = id;
         this.nombre = nombre;
         this.correo = correo;
         this.telefono = telefono;
     }
-
+    //Indica quienn realiza la compra
     public void realizarCompra() {
         System.out.println("Compra realizada por el cliente: " + nombre);
     }
-
+    //get y set
     public String getTelefono() {
         return telefono;
     }
@@ -25,11 +26,12 @@ public class Cliente {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+    //Indica quien y a quien se está llamando
 
     public void llamarCliente() {
         System.out.println("Llamando al cliente " + nombre + " al teléfono " + telefono);
     }
-
+    //Indica a quien se le envia el correo
     public void escribirCorreo() {
         System.out.println("Enviando correo a " + correo);
     }
